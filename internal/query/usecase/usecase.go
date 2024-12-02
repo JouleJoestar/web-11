b7a02e61302257478f9e9d0787942726c1b7b007
Timur <timurtein@gmail.com>
2024-12-02 21:35:08 +0300

    copy-paste commit

---
+++ internal/query/usecase/usecase.go
@@ -0,0 +1,19 @@
+package usecase
+
+import "web-11/internal/query/provider"
+
+type Usecase struct {
+	p *provider.Provider
+}
+
+func NewUsecase(p *provider.Provider) *Usecase {
+	return &Usecase{p: p}
+}
+
+func (u *Usecase) GetUser(name string) (string, error) {
+	return u.p.SelectUser(name)
+}
+
+func (u *Usecase) CreateUser(name string) error {
+	return u.p.InsertUser(name)
+}
